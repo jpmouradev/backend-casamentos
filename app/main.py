6adb0e2be2a4e32325c32ad0b38a01bc8f6b4146
João Pedro Moura <jpmourabraga@gmail.com>
2026-07-23 10:23:20 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://jpmouradev.github.io/casamento",
+        "https://jpmouradev.github.io",
     ],
     allow_credentials=False,
     allow_methods=["*"],
