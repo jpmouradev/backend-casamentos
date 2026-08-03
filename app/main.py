04792c22aaf5c22902fc221d38f89e528f3b6bfc
João Pedro <jpmourabraga@gmail.com>
2026-08-02 22:15:03 -0300

    Fix

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://jpmouradev.github.io",
+        "https://isadoraegabriel.github.io",
     ],
     allow_credentials=False,
     allow_methods=["*"],
